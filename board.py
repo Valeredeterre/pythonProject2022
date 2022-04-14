a1e9796e0eb4eb462b05c57b4357e9795e41539c
benameurcamil <benameurcamil@gmail.com>
2022-04-14 23:05:45 +0200

    Placement des pieces

--- board.py
+++ board.py
@@ -5,6 +5,19 @@ class Board:
 
     def initialize(self):
         board = [["X" for i in range(8)] for j in range(8)]
+        for i in range(8):
+            board[i][1] = "P"
+            board[i][6] = "P"
+
+        for i in range(0, 8, 7): # i = 0 then i = 0 + 7
+            board[0][i] = "T"
+            board[1][i] = "C"
+            board[2][i] = "F"
+            board[3][i] = "R"
+            board[4][i] = "D"
+            board[5][i] = "F"
+            board[6][i] = "C"
+            board[7][i] = "T"
         return board
 
     def __repr__(self):
@@ -14,7 +27,6 @@ class Board:
             for j in range(8):
                 str += self.board[j][k]
         return str
-
 
 board1 = Board()
 print(board1)
