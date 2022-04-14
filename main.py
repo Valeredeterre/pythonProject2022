5d477b59c82d5360889cff214cb96656030f3b50
benameurcamil <benameurcamil@gmail.com>
2022-04-14 16:23:18 +0200

    Initialisation Board

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("test1")  # Press Ctrl+F8 to toggle the breakpoint.
-# This is a sample Python script.
+class Board:
+    def __init__(self):
+        self.board = self.initialize()
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def initialize(self):
+        board = [["X" for i in range(8)] for j in range(8)]
