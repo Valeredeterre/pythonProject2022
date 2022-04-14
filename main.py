24b0a645ea3b3e7d9bf47ee295543d5dc6acc08f
benameurcamil <benameurcamil@gmail.com>
2022-04-14 16:52:58 +0200

    Initialisation Board

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ class Board:
 
     def initialize(self):
         board = [["X" for i in range(8)] for j in range(8)]
+        return board
+
+    def __repr__(self):
+        str = ""
+        for k in range(8):
+            self.board.append("\n")
+            for j in range (8):
+                str += self.board[j][k]
+        return str
+
+board1 = Board()
+print(board1)
