457f1ea290511871dd5fb172a9d4bc25755f4c28
benameurcamil <benameurcamil@gmail.com>
2022-04-14 23:28:04 +0200

    add get_case_value

--- board.py
+++ board.py
@@ -3,8 +3,24 @@ class Board:
     def __init__(self):
         self.board = self.initialize()
 
+    def get_case_value(self, x, y):
+        return self.board[x][y]
+
     def initialize(self):
         board = [["X" for i in range(8)] for j in range(8)]
+        for i in range(8):
+            board[i][1] = "P"
+            board[i][6] = "P"
+
+        for i in range(0, 8, 7): # i = 0 then i = 0 + 7
+            board[0][i] = "T"
+            board[1][i] = "C"
+            board[2][i] = "F"
+            board[3][i] = "R"
+            board[4][i] = "D"
+            board[5][i] = "F"
+            board[6][i] = "C"
+            board[7][i] = "T"
         return board
 
     def __repr__(self):
@@ -14,3 +30,8 @@ class Board:
             for j in range(8):
                 str += self.board[j][k]
         return str
+
+chessboard = Board()
+print(chessboard)
+case_value = Board.get_case_value(chessboard, 3, 0)
+print(case_value)
