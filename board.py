d233eecb3f8c0fe3943def1530da0c12572fc256
benameurcamil <benameurcamil@gmail.com>
2022-04-15 17:41:50 +0200

    fct case_status

--- board.py
+++ board.py
@@ -3,9 +3,6 @@ class Board:
     def __init__(self):
         self.board = self.initialize()
 
-    def get_case_value(self, x, y):
-        return self.board[x][y]
-
     def initialize(self):
         board = [["X" for i in range(8)] for j in range(8)]
         for i in range(8):
@@ -31,7 +28,19 @@ class Board:
                 str += self.board[j][k]
         return str
 
+    def get_case_value(self, x, y):
+        return self.board[x][y]
+
+    def case_status(self, x ,y): # a return value of 0 refers to an empty case
+        if (self.get_case_value(x, y) != "X"):
+            return 1
+        else:
+            return 0
+
+
 chessboard = Board()
 print(chessboard)
 case_value = Board.get_case_value(chessboard, 3, 0)
 print(case_value)
+case_status = Board.case_status(chessboard, 3, 3)
+print(case_status)
