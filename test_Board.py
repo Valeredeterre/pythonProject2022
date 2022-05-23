65c97b938a722ab8f15b0d78307e04ed53a1ccbc
benameurcamil <benameurcamil@gmail.com>
2022-05-24 00:52:50 +0200

    isdeplacement_fou basics unit test added

--- test_Board.py
+++ test_Board.py
@@ -3,15 +3,23 @@ from board import Board
 
 
 class TestIsDeplacementCavalier(unittest.TestCase):
-    def test_boolans(self):
+    def test_boolean_ans(self):
         self.assertAlmostEqual(Board.isdeplacement_cavalier(Board, 0, 0, 1, 2), 1)
         self.assertAlmostEqual(Board.isdeplacement_cavalier(Board, 0, 0, 0, 2), 0)
 
     def test_values(self):
         self.assertRaises(IndexError, Board.isdeplacement_cavalier, Board, 0, 0, -1, 2)
 
+class TestIsDeplacementFou(unittest.TestCase):
+    def test_boolean_ans(self):
+        self.assertAlmostEqual(Board.isdeplacement_fou(Board, 0, 0, 3, 3), 1)
+        self.assertAlmostEqual(Board.isdeplacement_fou(Board, 3, 3, 3, 3), 0)
+
+    def test_values(self):
+        self.assertRaises(IndexError, Board.isdeplacement_fou, Board, 0, 0, -1, -1)
+
 test = TestIsDeplacementCavalier()
-test.test_boolans()
+test.test_boolean_ans()
 test.test_values()
 
 
