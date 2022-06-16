43de8796ee72286fa07a463d232328dcbb2f44bb
Camil Benameur <103584333+camilbenameur@users.noreply.github.com>
2022-06-16 13:35:36 +0200

    Delete player.py

--- player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, pseudo, colour):
-        self.pseudo = pseudo
-        self.colour = colour
-        self.echec
-
-    def __repr__(self):
-        return self.pseudo
